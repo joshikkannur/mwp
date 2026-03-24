379d7ab29706a91c32ee7bcf8de90ae5937b3466
joshi <jkoyilerian@solvd.com>
2026-03-24 17:51:51 +0530

    title changes

--- wp-content/themes/twentysixteen/index.php
+++ wp-content/themes/twentysixteen/index.php
@@ -17,14 +17,14 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main mytestclass" role="main">
+		<main id="main" class="site-main mytestclasstw" role="main">
 
 		<?php if ( have_posts() ) : ?>
 
 			<?php if ( is_home() && ! is_front_page() ) : ?>
 			     
 				<header>
-					<h1 class="page-title screen-reader-text">Test Test <?php single_post_title(); ?></h1>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); echo "Test Test"; ?></h1>
 				</header>
 			<?php endif; ?>
 
