5aff61027a97f58a33d9ddfde792a774d6ae2e23
joshi <jkoyilerian@solvd.com>
2026-03-24 17:49:54 +0530

    title changes

--- wp-content/themes/twentysixteen/index.php
+++ wp-content/themes/twentysixteen/index.php
@@ -22,9 +22,9 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 
 			<?php if ( is_home() && ! is_front_page() ) : ?>
-			     Test 
+			     
 				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+					<h1 class="page-title screen-reader-text">Test Test <?php single_post_title(); ?></h1>
 				</header>
 			<?php endif; ?>
 
